19eba47c1ef2ce11172cbb16cb15ed67be25680e
Eric Sue <eric88@gmail.com>
2014-08-07 18:31:53 -0700

    Match against Regexp with the same encoding as str. Forces str to utf-8 in the case of dummy encodings.

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,9 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    # Handle dummy encodings by forcing them to UTF-8
+    str.force_encoding('utf-8') if str.encoding.dummy?
+      
+    # Use a regexp that matches str's encoding
+    str =~ Regexp.new("^.+$".encode!(str.encoding))
   end
 end
--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -5,7 +5,9 @@ require_relative '../lib/regexp_m17n'
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     Encoding.list.each do |enc|
-      assert(RegexpM17N.non_empty?('.'.encode(enc)))
+      test_str = '.'
+      enc.dummy? ? test_str.force_encoding(enc) : test_str.encode!(enc)
+      assert(RegexpM17N.non_empty?(test_str))
     end
   end
 end
